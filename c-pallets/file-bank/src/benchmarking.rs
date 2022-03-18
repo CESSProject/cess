8e330910b348a12c458134664ed80483c545695b
ytqaljn <2716693942@qq.com>
2022-03-18 11:30:06 +0800

    style: add file-map readme

--- c-pallets/file-bank/src/benchmarking.rs
+++ c-pallets/file-bank/src/benchmarking.rs
@@ -41,32 +41,6 @@ benchmarks! {
 		assert_eq!(1, 1);
 	}
 
-	update {
-		let caller: T::AccountId = whitelisted_caller();
-        let mut fileid: Vec<u8> = Vec::new();
-		fileid.push(1u8);	
-        let mut simihash: Vec<u8> = Vec::new();
-		simihash.push(3u8);
-		let fileinfo = FileInfo{
-			filename: "testname".as_bytes().to_vec(),
-			owner: caller.clone(),
-			filehash: "testfilehash".as_bytes().to_vec(),
-			similarityhash: "testsimihash".as_bytes().to_vec(),
-			ispublic: 1,
-			backups: 3,
-			creator: "testcreator".as_bytes().to_vec(),
-			filesize: 1234543,
-			keywords: "faild success well".as_bytes().to_vec(),
-			email: "2614815674@qq.com".as_bytes().to_vec(),
-			uploadfee: 0u32.into(),
-			downloadfee: 1u32.into(),
-			deadline: 12800,
-		};
-		<File<T>>::insert(fileid.clone(), fileinfo);
-	}: update(SystemOrigin::Signed(caller.clone()), fileid, 0u8, simihash)
-	verify {
-		assert_eq!(1, 1);
-	}
 }
 #[cfg(test)]
 mod tests {
@@ -78,7 +52,6 @@ mod tests {
 	fn test_benchmarks() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(Pallet::<Test>::test_benchmark_upload());
-		assert_ok!(Pallet::<Test>::test_benchmark_update());
 	});
 	}
 }
