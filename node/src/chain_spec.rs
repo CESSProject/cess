9b6c11ef3b5bb0a79a6817aee89c0cbef8daf210
Shaka <shakawwq@gmail.com>
2022-04-28 10:42:31 +0800

    update chain.rs and ocw (#20)
    * update: chain_spec
    
    * fix: update chain_spec
    
    * feat: offchain
    
    * fix: offchain call extension

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,6 +1,6 @@
 use cess_node_runtime::{
 	AccountId, AuthorityDiscoveryConfig, BabeConfig, BalancesConfig, Balance, CouncilConfig,
-	GenesisConfig, GrandpaConfig, Block,
+	GenesisConfig, GrandpaConfig, Block, IndicesConfig,
 	ImOnlineConfig, SessionConfig, Signature, StakingConfig, SessionKeys, SudoConfig, StakerStatus,
 	SystemConfig, TechnicalCommitteeConfig, wasm_binary_unwrap, MaxNominations, DOLLARS
 };
@@ -336,6 +336,7 @@ fn testnet_genesis(
 			// Configure endowed accounts with initial balance of 1 << 60.
 			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
 		},
+		indices: IndicesConfig { indices: vec![] },
 		session: SessionConfig {
 			keys: initial_authorities
 				.iter()
