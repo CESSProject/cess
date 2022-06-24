e0e95b654e5a87ea91555546ca8fe5c1795168c7
ytqaljn <2716693942@qq.com>
2022-06-23 18:40:12 -0700

    test: file-bank mock.rs

--- c-pallets/file-bank/src/tests.rs
+++ c-pallets/file-bank/src/tests.rs
@@ -24,25 +24,25 @@ use frame_support::{assert_ok, assert_noop};
 
 #[derive(Debug, Clone)]
 pub struct MockingFileInfo {
-    pub file_id: Vec<u8>,
-    pub filename: Vec<u8>,
     pub file_hash: Vec<u8>,
-    pub backups: u8,
     pub file_size: u64,
-    pub public: bool,
-    pub download_fee: u128,
+	pub block_num: u32,
+	pub scan_size: u32,
+	pub segment_size: u32,
+    pub miner_acc: AccountId,
+	pub miner_ip: Vec<u8>,
 }
 
 impl Default for MockingFileInfo {
     fn default() -> Self {
         MockingFileInfo {
-            file_id: vec![1],
-            filename: vec![65],
-            file_hash: vec![8, 8, 8, 8],
-            backups: 3,
-            file_size: 1024,
-            public: true,
-            download_fee: 100,
+            file_hash: vec![5,45,23,2,19,5,2],
+            file_size: 12,
+            block_num: 8,
+            scan_size: 3,
+            segment_size: 3,
+            miner_acc: mock::miner1(),
+            miner_ip: vec![66,55,44,33,11],
         }
     }
 }
@@ -54,20 +54,20 @@ fn buy_space_works() {
         let bal_before = Balances::free_balance(acc1);
         let space_gb = 10_u128;
         let lease_count = 10_u128;  // one month a lease
-        let max_price = 100_u128;
+        let max_price = 100_u64;
         let bn = Sys::block_number();
 
         assert_ok!(Sminer::add_available_space(1_048_576 * 1024 * space_gb));  // GB => MB
 
         UnitPrice::<Test>::put(max_price * 1000000000000 + 1);
-        assert_noop!(FileBank::buy_space(Origin::signed(acc1), space_gb, lease_count, max_price), Error::<Test>::ExceedExpectations);
+        assert_noop!(FileBank::buy_space(Origin::signed(acc1), space_gb, lease_count, max_price as u128), Error::<Test>::ExceedExpectations);
 
-        let unit_price = 100_u128;
+        let unit_price = 100_u64;
         UnitPrice::<Test>::put(unit_price);
-        assert_ok!(FileBank::buy_space(Origin::signed(acc1), space_gb, lease_count, max_price));
+        assert_ok!(FileBank::buy_space(Origin::signed(acc1), space_gb, lease_count, max_price as u128));
         // balance verify
-        let pay_amount = unit_price * (space_gb * 1024) * lease_count / 3;
-        assert_eq!(bal_before - pay_amount as u128, Balances::free_balance(acc1));
+        let pay_amount = unit_price * (space_gb as u64 * 1024) * lease_count as u64 / 3;
+        assert_eq!(bal_before - pay_amount, Balances::free_balance(acc1));
 
         let space_info = UserSpaceList::<Test>::try_get(acc1).unwrap().pop().unwrap();
         assert_eq!(space_gb * 1024 * 1_048_576, space_info.size); // MB unit
@@ -78,21 +78,29 @@ fn buy_space_works() {
         assert_eq!(uhsd.purchased_space, uhsd.remaining_space);
 
         let event = Sys::events().pop().expect("Expected at least one BuySpace to be found").event;
-        assert_eq!(mock::Event::from(Event::BuySpace { acc: acc1, size: space_gb * 1024, fee: pay_amount }), event);
+        assert_eq!(mock::Event::from(Event::BuySpace { acc: acc1, size: space_gb * 1024, fee: pay_amount as u64 }), event);
     });
 }
 
+fn upload_declaration_alias(account: AccountId, file_name: Vec<u8>, file_hash: Vec<u8>) -> DispatchResult {
+    FileBank::upload_declaration(
+        Origin::signed(account),
+        file_hash,
+        file_name,
+    )
+}
+
 fn upload_file_alias(account: AccountId, address: Vec<u8>, file_info: &MockingFileInfo) -> DispatchResult {
-    let MockingFileInfo { file_id, filename, file_hash, backups, file_size, public, download_fee } = file_info.clone();
+    let MockingFileInfo { file_hash, file_size, block_num, scan_size, segment_size, miner_ip } = file_info.clone();
     FileBank::upload(
         Origin::signed(account),
-        address,
-        filename,
-        file_id,
         file_hash,
-        public,
-        backups,
         file_size,
+        block_num,
+        scan_size,
+        segment_size,
+        backups,
+        miner_ip,
         download_fee,
     )
 }
@@ -141,38 +149,6 @@ fn upload_should_not_work_when_insufficient_storage() {
                 assert_eq!("InsufficientStorage", m.message.unwrap());
             }
         }
-    })
-}
-
-#[test]
-fn buy_file_works() {
-    new_test_ext().execute_with(|| {
-        let acc1 = mock::account1();
-        let mfi = MockingFileInfo::default();
-        let space_gb = 10_u128;
-        assert_ok!(Sminer::add_available_space(1_048_576 * 1024 * space_gb));
-        UnitPrice::<Test>::put(100);
-        assert_ok!(FileBank::buy_space(Origin::signed(acc1), space_gb, 100, 1000));
-        // acc1 upload file
-        assert_ok!(upload_file_alias(acc1, vec![1,1,1,1], &mfi));
-        let bounded_file_id: BoundedVec<u8, StringLimit> = mfi.file_id.to_vec().try_into().unwrap();
-        assert_eq!(acc1, File::<Test>::try_get(&bounded_file_id).unwrap().user_addr);
-
-        let acc2 = mock::account2();
-        let bal1_before = Balances::free_balance(&acc1);
-        let bal2_before = Balances::free_balance(&acc2);
-        let acc_comp = <Test as pallet::Config>::FilbakPalletId::get().into_account();
-        let bal_comp_before = Balances::free_balance(&acc_comp);
-
-        // acc2 buy file from acc1
-        assert_ok!(FileBank::buyfile(Origin::signed(acc2), mfi.file_id.clone(), vec![2,2,2,2]));
-
-        assert_eq!(bal2_before - mfi.download_fee, Balances::free_balance(&acc2));
-        assert_eq!(bal1_before + mfi.download_fee * 8 / 10, Balances::free_balance(&acc1));
-        assert_eq!(bal_comp_before + mfi.download_fee * 2 / 10, Balances::free_balance(&acc_comp));
-
-        let event = Sys::events().pop().expect("Expected at least one BuyFile to be found").event;
-        assert_eq!(mock::Event::from(Event::BuyFile { acc: acc2, money: mfi.download_fee, fileid: mfi.file_id }), event);
     })
 }
 
