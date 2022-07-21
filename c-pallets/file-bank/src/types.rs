bf74e9a1e1dca98eaba4995803472972f1337a10
ytqaljn <2716693942@qq.com>
2022-07-20 20:45:51 -0700

    feat: package struct info

--- c-pallets/file-bank/src/types.rs
+++ c-pallets/file-bank/src/types.rs
@@ -18,20 +18,18 @@ pub struct FileInfo<T: pallet::Config> {
 	pub(super) file_state: BoundedVec<u8, T::StringLimit>,
 }
 
-
-#[derive(PartialEq, Eq, Encode, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
-pub struct StorageSpace {
-	pub(super) purchased_space: u128,
-	pub(super) used_space: u128,
-	pub(super) remaining_space: u128,
-}
-
 #[derive(PartialEq, Eq, Encode, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
 #[scale_info(skip_type_params(T))]
 #[codec(mel_bound())]
-pub struct SpaceInfo<T: pallet::Config> {
-	pub(super) size: u128,
+pub struct PackageDetails<T: pallet::Config> {
+	pub(super) space: u128,
+	pub(super) used_space: u128,
+	pub(super) remaining_space: u128,
+	pub(super) tenancy: u32,
+	pub(super) package_type: u8,
+	pub(super) start: BlockNumberOf<T>,
 	pub(super) deadline: BlockNumberOf<T>,
+	pub(super) state: BoundedVec<u8, T::StringLimit>,
 }
 
 //Fill in file structure information
