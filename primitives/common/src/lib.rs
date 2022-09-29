cf1245161430a93a70ca683e91631fe81d72e722
ytqaljn <2716693942@qq.com>
2022-09-29 02:48:54 -0700

    fix: use enum for package type

---
+++ primitives/common/src/lib.rs
@@ -0,0 +1,30 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/* use */
+use frame_support::{
+	RuntimeDebug,
+	dispatch::{Decode, Encode},
+};
+use codec::{
+	MaxEncodedLen,
+};
+use scale_info::TypeInfo;
+
+pub struct Hash(pub [u8; 68]);
+
+impl Default for Hash {
+	fn default() -> Hash {
+		let new_hash = Hash([0u8; 68]);
+		return new_hash
+	}
+}
+
+#[derive(PartialEq, Eq, Encode, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
+pub enum PackageType {
+	Package1,
+	Package2,
+	Package3,
+	Package4,
+	Package5,
+}
+
