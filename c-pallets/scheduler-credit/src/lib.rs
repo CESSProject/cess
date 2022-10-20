6ecbac4d28d7391afb27f03c639ab0f5d81791f0
Shaka Lavigne <shakawwq@gmail.com>
2022-10-20 11:24:59 +0800

    feat: scheduler-credit

--- c-pallets/scheduler-credit/src/lib.rs
+++ c-pallets/scheduler-credit/src/lib.rs
@@ -6,10 +6,18 @@ mod mock;
 mod tests;
 
 use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::traits::ValidatorCredits;
+use frame_support::{
+	pallet_prelude::*,
+	traits::ValidatorCredits,
+	weights::Weight,
+};
 use log::{debug, warn};
 use scale_info::TypeInfo;
-use sp_runtime::RuntimeDebug;
+use sp_runtime::{
+	traits::{SaturatedConversion, Zero},
+	Percent,
+	RuntimeDebug,
+};
 use sp_std::{collections::btree_map::BTreeMap, prelude::*};
 
 use cp_scheduler_credit::{SchedulerCreditCounter, SchedulerStashAccountFinder};
@@ -21,6 +29,15 @@ pub type CreditScore = u32;
 
 pub const FULL_CREDIT_SCORE: u32 = 1000;
 const LOG_TARGET: &str = "scheduler-credit";
+/// The weight of credit value when figure credit score.
+/// period n-1 to period n-5
+const PERIOD_WEIGHT: [Percent; 5] = [
+	Percent::from_percent(50),
+	Percent::from_percent(20),
+	Percent::from_percent(15),
+	Percent::from_percent(10),
+	Percent::from_percent(5),
+];
 
 #[derive(PartialEq, Eq, Clone, Encode, Decode, TypeInfo, MaxEncodedLen, RuntimeDebug)]
 pub struct SchedulerCounterEntry {
@@ -63,13 +80,20 @@ impl Default for SchedulerCounterEntry {
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::pallet_prelude::*;
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(sp_std::marker::PhantomData<T>);
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
+
+		#[pallet::constant]
+		type PeriodDuration: Get<Self::BlockNumber>;
+
+		/// Number of periods to keep in history.
+		#[pallet::constant]
+		type HistoryDepth: Get<u32>;
+
 		type StashAccountFinder: SchedulerStashAccountFinder<Self::AccountId>;
 	}
 
@@ -77,6 +101,23 @@ pub mod pallet {
 	#[pallet::getter(fn current_scheduler_credits)]
 	pub(super) type CurrentCounters<T: Config> =
 		StorageMap<_, Blake2_128Concat, T::AccountId, SchedulerCounterEntry, ValueQuery>;
+
+	/// Stores the latest `HistoryDepth` preiods credit values.
+	#[pallet::storage]
+	pub(super) type HistoryCreditValues<T: Config> =
+		StorageDoubleMap<_, Twox64Concat, u32, Blake2_128Concat, T::AccountId, u32, ValueQuery>;
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {
+		fn on_initialize(now: T::BlockNumber) -> Weight {
+			let period_duration = T::PeriodDuration::get();
+			if now % period_duration == T::BlockNumber::zero() {
+				let period: u32 = (now / period_duration).saturated_into();
+				Self::figure_credit_values(period.saturating_sub(1));
+			}
+			0
+		}
+	}
 }
 
 impl<T: Config> Pallet<T> {
@@ -88,34 +129,58 @@ impl<T: Config> Pallet<T> {
 		<CurrentCounters<T>>::mutate(scheduler_id, |scb| scb.increase_punishment_count());
 	}
 
-	pub fn figure_credits() -> BTreeMap<T::AccountId, CreditScore> {
-		let mut credit_map = BTreeMap::new();
+	pub fn figure_credit_values(period: u32) {
 		let mut total_size = 0_u64;
-		for (ctrl_account_id, counter_entry) in <CurrentCounters<T>>::iter() {
+		for (_, counter_entry) in <CurrentCounters<T>>::iter() {
 			total_size += counter_entry.proceed_block_size;
-			if let Some(stash_account_id) =
-				T::StashAccountFinder::find_stash_account_id(&ctrl_account_id)
-			{
-				credit_map.insert(stash_account_id, counter_entry);
-			} else {
-				warn!(
-					target: LOG_TARGET,
-					"can not find the scheduler stash account for the controller account: {:?}",
-					ctrl_account_id
-				);
-			}
 		}
-		let mut result = BTreeMap::new();
-		for (stash_account_id, counter_entry) in credit_map {
+
+		for (ctrl_account_id, counter_entry) in <CurrentCounters<T>>::iter() {
 			let credit_score = counter_entry.figure_credit_score(total_size);
 			debug!(
 				target: LOG_TARGET,
 				"scheduler stash account: {:?}, credit: {}",
-				stash_account_id,
+				ctrl_account_id,
 				credit_score
 			);
-			result.insert(stash_account_id, credit_score);
+			HistoryCreditValues::<T>::insert(&period, &ctrl_account_id, credit_score);
 		}
+
+		//Remove `period - HistoryDepth` credit values in history.
+		let history_depth = T::HistoryDepth::get();
+		if period > history_depth {
+			HistoryCreditValues::<T>::remove_prefix(&period.saturating_sub(history_depth), None);
+		}
+	}
+
+	pub fn figure_credit_scores() -> BTreeMap<T::AccountId, CreditScore> {
+		let mut result = BTreeMap::new();
+		let now = <frame_system::Pallet<T>>::block_number();
+		let period_duration = T::PeriodDuration::get();
+		let period: u32 = (now / period_duration).saturated_into();
+		let last_period = period.saturating_sub(1);
+		HistoryCreditValues::<T>::iter_key_prefix(&last_period)
+			.for_each(|ctrl_account_id| {
+				if let Some(stash_account_id) =
+					T::StashAccountFinder::find_stash_account_id(&ctrl_account_id)
+				{
+					let mut credit_score = 0_u32;
+					for (index, weight) in PERIOD_WEIGHT.into_iter().enumerate() {
+						if last_period > index as u32 {
+							let credit_value = HistoryCreditValues::<T>::try_get(&last_period.saturating_sub(index as u32), &ctrl_account_id)
+								.unwrap_or(0);
+							credit_score += weight * credit_value;
+						}
+					}
+					result.insert(stash_account_id, credit_score);
+				} else {
+					warn!(
+						target: LOG_TARGET,
+						"can not find the scheduler stash account for the controller account: {:?}",
+						ctrl_account_id
+					);
+				}
+		});
 		result
 	}
 }
@@ -137,7 +202,7 @@ impl<T: Config> ValidatorCredits<T::AccountId> for Pallet<T> {
 
 	fn credits(epoch_index: u64) -> BTreeMap<T::AccountId, CreditScore> {
 		debug!(target: LOG_TARGET, "begin fetch schedulers credit on epoch: {}", epoch_index);
-		Pallet::<T>::figure_credits()
+		Pallet::<T>::figure_credit_scores()
 	}
 }
 
