040a401fd9c4c55b134fa657657fd149f60c60ec
Bill.W <wyi.net@gmail.com>
2022-10-21 16:16:59 +0800

    style: remove unused comment code

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -96,25 +96,6 @@ pub use sp_runtime::BuildStorage;
 
 mod voter_bags;
 
-/// An index to a block.
-// pub type BlockNumber = u32;
-
-// /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
-// pub type Signature = MultiSignature;
-
-// /// Some way of identifying an account on the chain. We intentionally make it equivalent
-// /// to the public key of our transaction signing scheme.
-// pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
-
-// /// Balance of an account.
-// pub type Balance = u128;
-
-// /// Index of a transaction in the chain.
-// pub type Index = u32;
-
-// /// A hash of some data used by the chain.
-// pub type Hash = sp_core::H256;
-
 pub type BlockNumber = u32;
 
 /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
