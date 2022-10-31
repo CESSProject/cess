58fa0346aa2fb5cf2fdf9051d4014244d0a18cf9
Bill.W <wyi.net@gmail.com>
2022-10-31 16:57:01 +0800

    style: remove unused type definitions (#93)
    * style:add readme for scheduler-credit pallet
    
    * style:improve documentation comments for scheduler credit primitives module
    
    * style: remove unused type definitions
    
    Co-authored-by: Elden Young <59600396+ytqaljn@users.noreply.github.com>

--- primitives/scheduler-credit/src/lib.rs
+++ primitives/scheduler-credit/src/lib.rs
@@ -1,13 +1,18 @@
+/*!
+# Some scheduler credit primitives
+*/
 #![cfg_attr(not(feature = "std"), no_std)]
 use frame_support::dispatch::DispatchResult;
 /// API necessary for Scheduler record ops about credit.
 pub trait SchedulerCreditCounter<SchedulerCtrlAccountId> {
-
+  /// Records the number of file bytes processed by the scheduler
 	fn record_proceed_block_size(scheduler_id: &SchedulerCtrlAccountId, block_size: u64) -> DispatchResult;
-
+  /// Record the number of times the scheduler has been punished
 	fn record_punishment(scheduler_id: &SchedulerCtrlAccountId) -> DispatchResult;
 }
 
+/// Stash account finder, used to find the corresponding Stash account according to the Controller account
 pub trait SchedulerStashAccountFinder<AccountId> {
+	/// find the corresponding Stash account according to the Controller account
 	fn find_stash_account_id(ctrl_account_id: &AccountId) -> Option<AccountId>;
 }
