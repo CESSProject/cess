a7f577e2ff30f01f513f4eb75031abd15153915b
Bill.W <wyi.net@gmail.com>
2022-10-31 16:58:05 +0800

    Improve documentation comments (#92)
    * style:add readme for scheduler-credit pallet
    
    * style:improve documentation comments for scheduler credit primitives module
    
    Co-authored-by: Elden Young <59600396+ytqaljn@users.noreply.github.com>

--- primitives/scheduler-credit/src/lib.rs
+++ primitives/scheduler-credit/src/lib.rs
@@ -5,10 +5,13 @@
 use frame_support::dispatch::DispatchResult;
 /// API necessary for Scheduler record ops about credit.
 pub trait SchedulerCreditCounter<SchedulerCtrlAccountId> {
+
   /// Records the number of file bytes processed by the scheduler
 	fn record_proceed_block_size(scheduler_id: &SchedulerCtrlAccountId, block_size: u64) -> DispatchResult;
+  
   /// Record the number of times the scheduler has been punished
 	fn record_punishment(scheduler_id: &SchedulerCtrlAccountId) -> DispatchResult;
+
 }
 
 /// Stash account finder, used to find the corresponding Stash account according to the Controller account
