35e97b21ee5518f885ba7ea28c648982a66dad12
Shaka Lavigne <shakawwq@gmail.com>
2022-11-18 17:59:56 +0800

    fix: deposit formula in runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -200,7 +200,7 @@ pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a c
 pub const DOLLARS: Balance = 100 * CENTS;
 
 pub const fn deposit(items: u32, bytes: u32) -> Balance {
-	items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
+	items as Balance * 15 * CENTS + (bytes as Balance) * 100 * MILLICENTS
 }
 
 /// Type used for expressing timestamp.
