0ea6b2957d699216c65bdf191d9d454510901830
ytqaljn <2716693942@qq.com>
2022-12-01 23:31:08 -0800

    test: bloom interface

--- primitives/common/src/lib.rs
+++ primitives/common/src/lib.rs
@@ -10,12 +10,16 @@ use scale_info::TypeInfo;
 
 #[derive(Copy, Clone, Eq, PartialEq, Encode, Decode, RuntimeDebug, MaxEncodedLen, TypeInfo)]
 pub struct Hash(pub [u8; 64]);
-pub struct TryFromSliceError(());
+pub enum HashError {
+	TryFromSliceError,
+	BinaryError,
+}
 
-impl sp_std::fmt::Debug for TryFromSliceError {
+impl sp_std::fmt::Debug for HashError {
 	fn fmt(&self, fmt: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
-		match *self {
-			_ => write!(fmt, "try form slice error!"),
+		match self {
+			TryFromSliceError => write!(fmt, "try form slice error!"),
+			BinaryError => write!(fmt, "convert binary error!"),
 		}
 	}
 }
@@ -71,17 +75,51 @@ impl Default for Hash {
 // }
 
 impl Hash {
-	pub fn slice_to_array_64(slice: &[u8]) -> Result<[u8; 64], TryFromSliceError> {
+	pub fn binary(&self) -> Result<[u8; 256], HashError> {
+		let mut elem: [u8; 256] = [0u8; 256];
+		let mut index: usize = 0;
+		for value in self.0.iter() {
+			let binary = match value {
+				b'1' => [0, 0, 0, 1],
+				b'2' => [0, 0, 1, 0],
+				b'3' => [0, 0, 1, 1],
+				b'4' => [0, 1, 0, 0],
+				b'5' => [0, 1, 0, 1],
+				b'6' => [0, 1, 1, 0],
+				b'7' => [0, 1, 1, 1],
+				b'8' => [1, 0, 0, 0],
+				b'9' => [1, 0, 0, 1],
+				b'a' => [1, 0, 1, 0],
+				b'b' => [1, 0, 1, 1],
+				b'c' => [1, 1, 0, 0],
+				b'd' => [1, 1, 0, 1],
+				b'e' => [1, 1, 1, 0],
+				b'f' => [1, 1, 1, 1],
+				_ 	 => return Err(HashError::BinaryError),
+			};
+
+			elem[index * 4] = binary[0];
+			elem[index * 4 + 1] = binary[1];
+			elem[index * 4 + 2] = binary[2];
+			elem[index * 4 + 3] = binary[3];
+
+			index = index + 1;
+		}
+		Ok(elem)
+
+	}
+
+	pub fn slice_to_array_64(slice: &[u8]) -> Result<[u8; 64], HashError> {
 		// log::info!("slice len: {:?}", slice.len());
 		if slice.len() == 64 {
-			let ptr: [u8; 64] = (*slice).try_into().map_err(|_e| TryFromSliceError(()))?;
+			let ptr: [u8; 64] = (*slice).try_into().map_err(|_e| HashError::TryFromSliceError)?;
 			Ok(ptr)
 		} else {
-			Err(TryFromSliceError(()))
+			Err(HashError::TryFromSliceError)
 		}
 	}
 
-	pub fn from_shard_id(shard_id: &[u8; 68]) -> Result<Self, TryFromSliceError> {
+	pub fn from_shard_id(shard_id: &[u8; 68]) -> Result<Self, HashError> {
 		let slice = Self::slice_to_array_64(&shard_id[0..64])?;
 		let hash = Hash(slice);
 		return Ok(hash)
