5e76ec7e9752cd6be3cb79be05011eb6e82f11a2
ytqaljn <2716693942@qq.com>
2022-12-14 00:35:25 -0800

    feat: new type  and new const

--- primitives/common/src/lib.rs
+++ primitives/common/src/lib.rs
@@ -24,55 +24,12 @@ impl sp_std::fmt::Debug for HashError {
 	}
 }
 
-// impl Encode for Hash {
-// 	// fn using_encoded<R, F>(&self, f: F) -> R
-// 	// 	where
-// 	// 		F: FnOnce(&[u8]) -> R
-// 	// {
-// 	// 	f(self.0.as_slice())
-// 	// }
-// 	//
-// 	// fn encode(&self) -> Vec<u8> {
-// 	// 	self.0.encode()
-// 	// }
-// 	fn encode_to<T: Output + ?Sized>(&self, dest: &mut T) {
-// 		for i in self.0.iter() {
-// 			dest.push_byte(*i)
-// 		}
-// 	}
-// }
-
-// impl EncodeLike<Hash> for Hash {}
-
-// impl Decode for Hash {
-// 	fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
-// 		let value: &mut [u8] = Default::default();
-// 		let _ = input.read(value)?;
-// 		let v = Hash::slice_to_array_68(value).map_err(|_| "convert err")?;
-// 		let hash = Hash(v);
-// 		return Ok(hash)
-// 	}
-// }
-
 impl Default for Hash {
 	fn default() -> Hash {
 		let new_hash = Hash([0u8; 64]);
 		return new_hash
 	}
 }
-
-// impl TypeInfo for Hash {
-// 	type Identity = Self;
-//
-// 	fn type_info() -> scale_info::Type {
-// 		scale_info::Type::builder()
-// 			.path(scale_info::Path::new("Vote", module_path!()))
-// 			.composite(
-// 				scale_info::build::Fields::unnamed()
-// 					.field(|f| f.ty::<[u8; 68]>()),
-// 			)
-// 	}
-// }
 
 impl Hash {
 	pub fn binary(&self) -> Result<[u8; 256], HashError> {
@@ -108,6 +65,11 @@ impl Hash {
 		}
 		Ok(elem)
 
+	}
+
+	pub fn new(slice: &[u8]) -> Result<Self, HashError> {
+		let array = Self::slice_to_array_64(slice)?;
+		Ok(Hash(array))
 	}
 
 	pub fn slice_to_array_64(slice: &[u8]) -> Result<[u8; 64], HashError> {
@@ -150,3 +112,14 @@ pub enum IpAddress {
 	IPV6([u16; 8], u16),
 }
 
+pub type Mrenclave = [u8; 32];
+
+pub const M_BYTE: u128 = 1_048_576;
+
+pub const G_BYTE: u128 = 1_048_576 * 1024;
+
+pub const T_BYTE: u128 = 1_048_576 * 1024 * 1024;
+
+pub const SLICE_DEFAULT_SIZE_MUTI: u128 = 512;
+
+pub const SLICE_DEFAULT_BYTE: u128 = SLICE_DEFAULT_SIZE_MUTI * M_BYTE;
