2ed70cb31936164dd7e2cb3094f7c6bffa308d2b
ytqaljn <2716693942@qq.com>
2022-12-14 00:35:45 -0800

    feat: miner struct update

--- c-pallets/sminer/src/types.rs
+++ c-pallets/sminer/src/types.rs
@@ -7,47 +7,59 @@ use frame_support::pallet_prelude::MaxEncodedLen;
 type BlockNumberOf<T> = <T as frame_system::Config>::BlockNumber;
 /// The custom struct for storing info of storage miners.
 #[derive(PartialEq, Eq, Encode, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
+#[scale_info(skip_type_params(T))]
 pub struct MinerInfo<AccountId, Balance, BoundedString> {
-	pub(super) peer_id: u64,
-	//Income account
 	pub(super) beneficiary: AccountId,
 	pub(super) ip: IpAddress,
 	pub(super) collaterals: Balance,
-	//nomal, exit, frozen, e_frozen
+	pub(super) debt: Balance,
+	//nomal, exit, frozen, e_frozen, debt
 	pub(super) state: BoundedString,
-	pub(super) power: u128,
-	pub(super) space: u128,
-	pub(super) reward_info: RewardInfo<Balance>,
+	pub(super) idle_space: u128,
+	pub(super) service_space: u128,
+	pub(super) autonomy_space: u128,
+	pub(super) puk: Public,
+	pub(super) ias_cert: IasCert,
+	pub(super) bloom_filter: BloomCollect,
 }
 
 #[derive(PartialEq, Eq, Encode, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
-pub struct RewardInfo<Balance> {
-    pub(super) total_reward: Balance,
-	pub(super) total_rewards_currently_available: Balance,
-	pub(super) total_not_receive: Balance,
+pub struct RewardOrder<Balance> {
+	//Total award for orders
+	order_reward: Balance,
+	//Number of awards to be distributed each time
+	each_share: Balance,
+	//Number of order reward distribution
+	award_count: u8,
+	//Whether the 20% reward immediately paid has been paid
+	has_issued: bool,
 }
-/// The custom struct for storing info of storage CalculateRewardOrder.
-#[derive(PartialEq, Eq, Encode, Default, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
-#[scale_info(skip_type_params(T))]
-#[codec(mel_bound())]
-pub struct CalculateRewardOrder<T: pallet::Config> {
-	pub(super) calculate_reward: u128,
-	pub(super) start_t: BlockNumberOf<T>,
-	pub(super) deadline: BlockNumberOf<T>,
-}
-/// The custom struct for storing info of storage RewardClaim.
+
 #[derive(PartialEq, Eq, Encode, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
-pub struct RewardClaim<AccountId, Balance> {
-	pub(super) beneficiary: AccountId,
-	pub(super) total_reward: Balance,
-	pub(super) have_to_receive: Balance,
-	pub(super) current_availability: Balance,
-	pub(super) total_not_receive: Balance,
+pub struct Reward<Balance> {
+	//Total reward for miners
+    total_reward: Balance,
+	//Rewards issued at present
+	reward_issued: Balance,
+	//Reward order list, up to 180 reward orders can be accumulated
+	order_list: [RewardOrder<Balance>; 180],
 }
+
+
+
 /// The custom struct for storing info of storage FaucetRecord.
 #[derive(PartialEq, Eq, Encode, Default, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
 pub struct FaucetRecord<BlockNumber> {
 	pub(super) last_claim_time: BlockNumber,
 }
 
+#[derive(PartialEq, Eq, Encode, Default, Decode, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo)]
+pub struct BloomCollect {
+	//for autonomy file
+	pub(super) autonomy_filter: BloomFilter,
+	//for service file
+	pub(super) service_filter: BloomFilter,
+	//for filler file
+	pub(super) idle_filter: BloomFilter,
+}
 
