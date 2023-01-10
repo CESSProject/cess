252baa50daee1582c37d27db39dc1a3d147e9c3e
ytqaljn <2716693942@qq.com>
2023-01-09 23:31:19 -0800

    fix: challenge json parse

--- c-pallets/segment-book/src/impls.rs
+++ c-pallets/segment-book/src/impls.rs
@@ -1,75 +1,64 @@
 use super::*;
-use serde_json::Value;
+
 use sp_std::str::FromStr;
 
 impl<T: pallet::Config> ChallengeReport<T> {
     pub fn parse(&self) -> Result<ReportMessage<T>, DispatchError> {
+        log::info!("start!");
         let body: Value = serde_json::from_slice(&self.message).map_err(|_| Error::<T>::ParseError)?;
-
-        let idle_str = body["idleFilter"].as_str().ok_or(Error::<T>::ParseError)?;
-        let idle_str = idle_str.split(|elem| elem == ',');
-        let mut index = 0;
-        let mut idle_filter = BloomFilter::default();
-        for elem in idle_str {
-            idle_filter.0[index] = u64::from_str(elem).map_err(|_| Error::<T>::ParseError)?;
-            index = index + 1;
-        }
-
-        let binding = body["serviceFilter"].as_str().ok_or(Error::<T>::ParseError)?;
-        let service_str = binding.split(|elem| elem == ',');
-        let mut index = 0;
-        let mut service_filter = BloomFilter::default();
-        for elem in service_str {
-            service_filter.0[index] = u64::from_str(elem).map_err(|_| Error::<T>::ParseError)?;
-            index = index + 1;
-        }
-
-        let binding = body["autonomyFilter"].as_str().ok_or(Error::<T>::ParseError)?;
-        let autonomy_str = binding.split(|elem| elem == ',');
-        let mut index = 0;
-        let mut autonomy_filter = BloomFilter::default();
-        for elem in autonomy_str {
-            autonomy_filter.0[index] = u64::from_str(elem).map_err(|_| Error::<T>::ParseError)?;
-            index = index + 1;
-        }
-
-        let binding = body["random"].as_str().ok_or(Error::<T>::ParseError)?;
-        let random_bytes = binding.as_bytes();
+        log::info!("---1---");
+        let binding = body["idle_bloom_filter"].as_array().ok_or(Error::<T>::ParseError)?;
+        let idle_filter_array = Self::parse_array(binding)?;
+        let idle_filter = BloomFilter(idle_filter_array);
+        log::info!("---2---");
+        let binding = body["service_bloom_filter"].as_array().ok_or(Error::<T>::ParseError)?;
+        let service_filter_array = Self::parse_array(binding)?;
+        let service_filter = BloomFilter(service_filter_array);
+        log::info!("---2---");
+        let binding = body["autonomous_bloom_filter"].as_array().ok_or(Error::<T>::ParseError)?;
+        let autonomy_filter_array = Self::parse_array(binding)?;
+        let autonomy_filter = BloomFilter(autonomy_filter_array);
+        log::info!("---4---");
+        let binding = body["chal_id"].as_str().ok_or(Error::<T>::ParseError)?;
+        let random_bytes: Vec<u8> = match base64::decode(binding) {
+            Ok(value) => value,
+            Err(_) => Err(Error::<T>::ParseError)?,
+        };
         let random: [u8; 20] = random_bytes.try_into().map_err(|_| Error::<T>::ParseError)?;
-
+        log::info!("---5---");
         let mut failed_idle_file: Vec<Hash> = Default::default();
-        let binding = body["failedIdleFile"].as_str().ok_or(Error::<T>::ParseError)?;
+        let binding = body["idle_failed_file_hashes"].as_str().ok_or(Error::<T>::ParseError)?;
         let failed_bytes = binding.as_bytes();
         if failed_bytes.len() != 0 {
-            let failed_arr = failed_bytes.split(|elem| elem == &b'/');
+            let failed_arr = failed_bytes.split(|elem| elem == &b'|');
             for elem in failed_arr {
                 let hash_temp = Hash::new(elem).map_err(|_| Error::<T>::ParseError)?;
                 failed_idle_file.push(hash_temp);
             }
         }
-        
+        log::info!("---6---");
         let mut failed_service_file: Vec<[u8; 68]> = Default::default();
-        let binding = body["failedServiceFile"].as_str().ok_or(Error::<T>::ParseError)?;
+        let binding = body["service_failed_file_hashes"].as_str().ok_or(Error::<T>::ParseError)?;
         let failed_bytes = binding.as_bytes();
         if failed_bytes.len() != 0 {
-            let failed_arr = failed_bytes.split(|elem| elem == &b'/');
+            let failed_arr = failed_bytes.split(|elem| elem == &b'|');
             for elem in failed_arr {
                 let share_id_temp: [u8; 68] = elem.try_into().map_err(|_| Error::<T>::ParseError)?;
                 failed_service_file.push(share_id_temp);
             }
         }
-
+        log::info!("---7---");
         let mut failed_autonomy_file: Vec<Hash> = Default::default();
-        let binding = body["failedAutonomyFile"].as_str().ok_or(Error::<T>::ParseError)?;
+        let binding = body["autonomous_failed_file_hashes"].as_str().ok_or(Error::<T>::ParseError)?;
         let failed_bytes = binding.as_bytes();
         if failed_bytes.len() != 0 {
-            let failed_arr = failed_bytes.split(|elem| elem == &b'/');
+            let failed_arr = failed_bytes.split(|elem| elem == &b'|');
             for elem in failed_arr {
                 let hash_temp = Hash::new(elem).map_err(|_| Error::<T>::ParseError)?;
                 failed_autonomy_file.push(hash_temp);
             }
         }
-
+        log::info!("---8---");
 
         let report = ReportMessage::<T> {
             idle_filter: idle_filter,
@@ -80,7 +69,26 @@ impl<T: pallet::Config> ChallengeReport<T> {
             failed_service_file: failed_service_file.try_into().map_err(|_| Error::<T>::BoundedVecError)?,
             failed_autonomy_file: failed_autonomy_file.try_into().map_err(|_| Error::<T>::BoundedVecError)?,
         };
-
+        log::info!("---9---");
         Ok(report)
     }
+
+    pub fn parse_array(binding: &Vec<Value>) -> Result<[u64; 256], DispatchError> {
+		log::info!("comp length");
+		ensure!(binding.len() == 256, Error::<T>::ParseError);
+        log::info!("---comp success---");
+		let mut array = [0u64; 256];
+		let mut index = 0;
+		for elem in binding {
+			let temp = match elem {
+				Value::Number(v) => v,
+				_ => Err(Error::<T>::ParseError)?,
+			};
+			array[index] = temp.as_u64().ok_or(Error::<T>::ParseError)?;
+            index = index + 1;
+		}
+		log::info!("array = {:?}",array);
+	
+		Ok(array)
+	}
 }
