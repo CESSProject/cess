e8507f376c56e5645001439fba89af2f82099950
Elden Young <59600396+ytqaljn@users.noreply.github.com>
2023-03-27 10:35:39 +0800

    style: delete comment (#118)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -157,11 +157,6 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("cess-node"),
 	impl_name: create_runtime_str!("cess-node"),
 	authoring_version: 1,
-	// The version of the runtime specification. A full node will not attempt to use its native
-	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
-	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
-	//   the compatible custom types.
 	spec_version: 101,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
