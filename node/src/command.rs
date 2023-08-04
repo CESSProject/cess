6817d0abe3cccf03a32f0993e34b77a643d2a4de
Anthony Chan <cbf0311@sina.com>
2023-08-04 10:13:32 +0800

    Update command.rs

--- node/src/command.rs
+++ node/src/command.rs
@@ -3,7 +3,7 @@
 // Copyright (C) 2017-2022 Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
-// This program is free a software: you can redistribute it and/or modify
+// This program is a free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
