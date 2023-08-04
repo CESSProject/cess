6b930e4331afd2752e2c7851f9f014249b0b5b0f
Dean Clark <32699199+thirteenggh@users.noreply.github.com>
2023-08-04 10:23:34 +0800

    Update sha1.rs

--- utils/ring/src/digest/sha1.rs
+++ utils/ring/src/digest/sha1.rs
@@ -1,6 +1,5 @@
 // Copyright 2015-2016 Brian Smith.
 // Copyright 2016 Simon Sapin.
-//
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
 // copyright notice and this permission notice appear in all copies.
