06bae142ba9f33143adc65556802c31c6f9f5b6b
ytqaljn <2716693942@qq.com>
2023-08-23 02:37:52 -0700

    feat: storage handler pallet benchmark

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -172,7 +172,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 104,
+	spec_version: 105,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -1592,6 +1592,7 @@ mod benches {
 		[pallet_sminer, SminerBench::<Runtime>]
 		// [pallet_oss, Oss]
 		[pallet_cacher, Cacher]
+		[pallet_storage_handler, StorageHandler]
 		[pallet_file_bank, FileBankBench::<Runtime>]
 		[pallet_tee_worker, TeeWorkerBench::<Runtime>]
 		// [pallet_audit, Audit]
