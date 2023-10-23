cb8f6d2bae2377cd5b0f93380c0fbb1b9859b794
ytqaljn <2716693942@qq.com>
2023-10-23 01:15:52 -0700

    feat: temp save

--- c-pallets/sminer-reward/src/lib.rs
+++ c-pallets/sminer-reward/src/lib.rs
@@ -2,21 +2,27 @@
 use frame_support::{
 	traits::{
 		Currency, ReservableCurrency,
+		ExistenceRequirement::KeepAlive,
 	},
-	dispatch::{DispatchResult},
-    pallet_prelude::{StorageValue, ValueQuery},
+	dispatch::{DispatchResult}, PalletId,
+    pallet_prelude::{Weight, StorageValue, ValueQuery, Get},
 };
 // use sp_std::prelude::*;
 use sp_runtime::{
     SaturatedConversion,
-	traits::{CheckedAdd, CheckedSub},
+	traits::{CheckedAdd, CheckedSub, AccountIdConversion},
 };
-// use frame_system::pallet_prelude::*;
+use frame_system::{
+	pallet_prelude::OriginFor,
+	ensure_signed,
+};
 
 pub use pallet::*;
 
 type BalanceOf<T> =
 	<<T as pallet::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+type AccountOf<T> = <T as frame_system::Config>::AccountId;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -43,6 +49,64 @@ pub mod pallet {
     #[pallet::storage]
 	#[pallet::getter(fn currency_reward)]
 	pub(super) type CurrencyReward<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+
+		#[pallet::call_index(0)]
+		// FIX ME!
+		#[pallet::weight(Weight::zero())]
+		pub fn send_funds_to_pid(
+			origin: OriginFor<T>,
+			funds: BalanceOf<T>,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			Self::send_to_pid(sender, funds)?;
+
+			Ok(())
+		}
+
+		#[pallet::call_index(1)]
+		// FIX ME!
+		#[pallet::weight(Weight::zero())]
+		pub fn send_funds_to_sid(
+			origin: OriginFor<T>,
+			funds: BalanceOf<T>,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			Self::send_to_sid(sender, funds)?;
+
+			Ok(())
+		}
+
+		#[pallet::call_index(2)]
+		// FIX ME!
+		#[pallet::weight(Weight::zero())]
+		pub fn pid_burn_funds(
+			origin: OriginFor<T>,
+			burn_amount: BalanceOf<T>,
+		) -> DispatchResult {
+			let _ = ensure_root(origin)?;
+
+			let pid = T::PunishTreasuryId::get().into_account_truncating();
+
+			Ok(())
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		fn send_to_pid(acc: AccountOf<T>, amount: BalanceOf<T>) -> DispatchResult {
+			let pid = T::PunishTreasuryId::get().into_account_truncating();
+			<T as pallet::Config>::Currency::transfer(&acc, &pid, amount, KeepAlive)
+		}
+	
+		fn send_to_sid(acc: AccountOf<T>, amount: BalanceOf<T>) -> DispatchResult {
+			let sid = T::SpaceTreasuryId::get().into_account_truncating();
+			<T as pallet::Config>::Currency::transfer(&acc, &sid, amount, KeepAlive)
+		}
+	}
 }
 
 pub trait RewardPool<Balance> {
@@ -85,8 +149,16 @@ impl<T: Config> RewardPool<BalanceOf<T>> for Pallet<T> {
 }
 
 pub trait TreasuryHandle<AccountId, Balance> {
-	fn send_to_punishid(acc: AccountId, amount: Balance) -> DispatchResult;
-	fn send_to_spaceid(acc: AccountId, amount: Balance) -> DispatchResult;
+	fn send_to_pid(acc: AccountId, amount: Balance) -> DispatchResult;
+	fn send_to_sid(acc: AccountId, amount: Balance) -> DispatchResult;
 }
 
-impl TreasuryHandle
+impl<T: Config> TreasuryHandle<AccountOf<T>, BalanceOf<T>> for Pallet<T> {
+	fn send_to_pid(acc: AccountOf<T>, amount: BalanceOf<T>) -> DispatchResult {
+		Self::send_to_pid(acc, amount)
+	}
+
+	fn send_to_sid(acc: AccountOf<T>, amount: BalanceOf<T>) -> DispatchResult {
+		Self::send_to_sid(acc, amount)
+	}
+}
