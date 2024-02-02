bb4686f84f8b3541ee94041f0685ff78a549c2e6
DemosChiang <1239865849@qq.com>
2024-02-02 15:31:38 +0800

    feat:using streaming for query tag instead of send directly

--- crates/cestory/src/podr2/proxy.rs
+++ crates/cestory/src/podr2/proxy.rs
@@ -1,10 +1,14 @@
+use std::pin::Pin;
+
 use super::Podr2Result;
 use crate::expert::{CesealExpertStub, ExternalResourceKind};
 use cestory_api::podr2::{
     podr2_api_server::Podr2Api, podr2_verifier_api_server::Podr2VerifierApi, EchoMessage, RequestBatchVerify,
     RequestGenTag, ResponseBatchVerify, ResponseGenTag,
 };
-use tonic::Request;
+use tokio_stream::Stream;
+use tonic::{Request, Response, Status, Streaming};
+pub(crate) type ResponseStream = Pin<Box<dyn Stream<Item = Result<ResponseGenTag, Status>> + Send>>;
 
 pub struct Podr2ApiServerProxy<Svc> {
     pub(crate) inner: Svc,
@@ -16,12 +20,23 @@ impl<Svc> Podr2Api for Podr2ApiServerProxy<Svc>
 where
     Svc: Podr2Api,
 {
-    async fn request_gen_tag(&self, request: Request<RequestGenTag>) -> Podr2Result<ResponseGenTag> {
+    #[allow(non_camel_case_types)]
+    type request_gen_tagStream = ResponseStream;
+    async fn request_gen_tag(
+        &self,
+        request: Request<Streaming<RequestGenTag>>,
+    ) -> Podr2Result<Self::request_gen_tagStream> {
         let _permit = self
             .ceseal_expert
             .try_acquire_permit(ExternalResourceKind::Pord2Service)
             .await?;
-        self.inner.request_gen_tag(request).await
+        match self.inner.request_gen_tag(request).await {
+            Ok(result) => {
+                let a = result.into_inner();
+                Ok(Response::new(Box::pin(a) as Self::request_gen_tagStream))
+            },
+            Err(fail) => Err(fail),
+        }
     }
 
     async fn echo(&self, request: Request<EchoMessage>) -> Podr2Result<EchoMessage> {
