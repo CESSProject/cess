aeaaa8f4ba2d90fefcec653cdd74ae084e0eadd8
ytqaljn <2716693942@qq.com>
2024-02-05 14:21:20 +0800

    fix: fix build featrures benchmark error

--- standalone/chain/node/src/benchmarking.rs
+++ standalone/chain/node/src/benchmarking.rs
@@ -119,6 +119,7 @@ pub fn create_benchmark_extrinsic(
 		)),
 		frame_system::CheckNonce::<runtime::Runtime>::from(nonce),
 		frame_system::CheckWeight::<runtime::Runtime>::new(),
+		ces_pallet_mq::CheckMqSequence::<runtime::Runtime>::new(),
 		pallet_transaction_payment::ChargeTransactionPayment::<runtime::Runtime>::from(0),
 	);
 
@@ -131,6 +132,7 @@ pub fn create_benchmark_extrinsic(
 			runtime::VERSION.transaction_version,
 			genesis_hash,
 			best_hash,
+			(),
 			(),
 			(),
 			(),
