ab560b6ae79afef82a7061684834e56d8b75845b
Demos Chiang <69138672+democ98@users.noreply.github.com>
2024-04-23 11:01:13 +0800

    perf:optimize pois algorithm (#328)
    * feat:add new_challenge_handle function
    
    * fix:some bug
    
    * use new challenge handle verify  space total
    
    * fix
    
    * fix:update some bug
    
    * debug log
    
    * debug
    
    * fix
    
    * add test
    
    * fix:bug
    
    * debug info added
    
    * add pois debug info
    
    * fix
    
    * fix:update pois

--- crates/ces-pois/src/pois/mod.rs
+++ crates/ces-pois/src/pois/mod.rs
@@ -1,2 +1,3 @@
+pub mod challenge;
 pub mod prove;
 pub mod verify;
--- crates/ces-pois/src/util/mod.rs
+++ crates/ces-pois/src/util/mod.rs
@@ -1,36 +1,56 @@
+use std::fs;
+
 use crate::acc::{self, RsaKey};
+use anyhow::Result;
 use num_bigint_dig::BigUint;
-use std::{fs, io::Result};
 
 pub fn copy_data(target: &mut [u8], src: &[&[u8]]) {
-	let mut count = 0;
-	let lens = target.len();
+    let mut count = 0;
+    let lens = target.len();
 
-	for d in src {
-		let l = d.len();
-		if l == 0 || l + count > lens {
-			continue
-		}
-		target[count..count + l].copy_from_slice(d);
-		count += l;
-	}
+    for d in src {
+        let l = d.len();
+        if l == 0 || l + count > lens {
+            continue;
+        }
+        target[count..count + l].copy_from_slice(d);
+        count += l;
+    }
 }
 
 pub fn parse_key(path: &str) -> Result<RsaKey> {
-	let data = fs::read(path)?;
-	Ok(get_key_from_bytes(&data))
+    let data = fs::read(path)?;
+    Ok(get_key_from_bytes(&data))
 }
 
 fn get_key_from_bytes(data: &[u8]) -> RsaKey {
-	if data.len() < 8 {
-		return acc::rsa_keygen(2048)
-	}
-	let nl = u64::from_be_bytes(data[..8].try_into().unwrap());
-	let gl = u64::from_be_bytes(data[8..16].try_into().unwrap());
-	if nl == 0 || gl == 0 || data.len() - 16 != (nl + gl) as usize {
-		return acc::rsa_keygen(2048)
-	}
-	let n = BigUint::from_bytes_be(&data[16..16 + nl as usize]);
-	let g = BigUint::from_bytes_be(&data[16 + nl as usize..]);
-	RsaKey::new(n, g)
+    if data.len() < 8 {
+        return acc::rsa_keygen(2048);
+    }
+    let nl = u64::from_be_bytes(data[..8].try_into().unwrap());
+    let gl = u64::from_be_bytes(data[8..16].try_into().unwrap());
+    if nl == 0 || gl == 0 || data.len() - 16 != (nl + gl) as usize {
+        return acc::rsa_keygen(2048);
+    }
+    let n = BigUint::from_bytes_be(&data[16..16 + nl as usize]);
+    let g = BigUint::from_bytes_be(&data[16 + nl as usize..]);
+    RsaKey::new(n, g)
+}
+
+pub fn add_data(target: &mut [u8], src: &[&[u8]]) {
+    let target_len = target.len();
+    for s in src {
+        if s.len() < target_len {
+            continue;
+        }
+        for (i, elem) in target.iter_mut().enumerate() {
+            *elem ^= s[i];
+        }
+    }
+}
+
+pub fn clear_data(target: &mut [u8]) {
+    for element in target.iter_mut() {
+        *element = 0;
+    }
 }
