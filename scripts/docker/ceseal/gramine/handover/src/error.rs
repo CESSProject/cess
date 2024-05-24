256ac0387ea7130002d4c88836a859aa7548076c
Demos Chiang <69138672+democ98@users.noreply.github.com>
2024-05-24 10:24:39 +0800

    refractor:handover moudule and fix some bug (#1)
    * refactor:key handover
    
    * fix:Fix handover errors and correct logic

---
+++ scripts/docker/ceseal/gramine/handover/src/error.rs
@@ -0,0 +1,24 @@
+use std::{error, fmt};
+
+#[derive(Debug)]
+pub enum Error {
+	StartCesealFailed(String),
+    RedirectCesealLogFailed(String),
+    DetectCesealRunningStatueFailed(String),
+    PreviousVersionFailed(String),
+	CopyDirectory(String)
+}
+
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Error::StartCesealFailed(e) => write!(f, "{:?}", e),
+            Error::RedirectCesealLogFailed(e) => write!(f, "{:?}", e),
+            Error::DetectCesealRunningStatueFailed(e) => write!(f, "{:?}", e),
+			Error::PreviousVersionFailed(e) => write!(f, "{:?}", e),
+			Error::CopyDirectory(e) => write!(f, "{:?}", e),
+		}
+	}
+}
+
+impl error::Error for Error {}
