51c8ebfe801b2a8bb6cf9511e8a9a126b6b31b58
ytqaljn <2716693942@qq.com>
2024-07-16 13:59:42 +0800

    update clear miner count

--- pallets/audit/src/lib.rs
+++ pallets/audit/src/lib.rs
@@ -897,7 +897,7 @@ pub mod pallet {
 							weight = weight.saturating_add(T::DbWeight::get().reads_writes(1, 1));
 						}
 
-						if count >= 30 {
+						if count >= 50 {
 							let result = T::MinerControl::force_miner_exit(&miner);
 							weight = weight.saturating_add(T::DbWeight::get().reads_writes(5, 5));
 							if result.is_err() {
