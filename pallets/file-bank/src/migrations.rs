4d1aa7124c17eac58acff23fa08c59f6fbf6b7af
ytqaljn <2716693942@qq.com>
2024-11-20 16:00:51 +0800

    feat: step migration

--- pallets/file-bank/src/migrations.rs
+++ pallets/file-bank/src/migrations.rs
@@ -1,6 +1,74 @@
 use super::*;
 use frame_support::traits::OnRuntimeUpgrade;
 use sp_runtime::{TryRuntimeError, Saturating};
+use frame_support::{
+	storage_alias, weights::WeightMeter,
+	migrations::{MigrationId, SteppedMigration, SteppedMigrationError},
+};
+
+pub const PALLET_MIGRATIONS_ID: &[u8; 26] = b"pallet-file-bank-migration";
+
+pub struct SteppedFileBank<T: Config, W: weights::WeightInfo>(PhantomData<(T, W)>);
+impl<T: Config, W: weights::WeightInfo> SteppedMigration for SteppedFileBank<T, W> {
+	type Cursor = Hash;
+
+	type Identifier = MigrationId<26>;
+
+	fn id() -> Self::Identifier {
+		MigrationId { pallet_id: *PALLET_MIGRATIONS_ID, version_from: 2, version_to: 3 }
+	}
+
+	fn step(
+		mut cursor: Option<Self::Cursor>, 
+		meter: &mut WeightMeter
+	) -> Result<Option<Self::Cursor>, SteppedMigrationError> {
+		let required = Weight::zero();
+
+		if meter.remaining().any_lt(required) {
+			return Err(SteppedMigrationError::InsufficientWeight { required });
+		}
+
+		loop {
+			if meter.try_consume(required).is_err() {
+				break;
+			}
+
+			let mut iter = if let Some(last_key) = cursor {
+				v3::File::<T>::iter_from(File::<T>::hashed_key_for(last_key))
+			} else {
+				v3::File::<T>::iter()
+			};
+
+			if let Some((last_key, value)) = iter.next() {
+				let mut new_owner: BoundedVec<UserBrief<T>, T::OwnerLimit> = Default::default();
+				for owner in value.owner {
+					new_owner.try_push(UserBrief::<T>{
+						user: owner.user,
+						file_name: owner.file_name,
+						territory_name: owner.territory_name,
+					}).unwrap();
+				}
+
+				File::<T>::try_mutate(last_key, |info_opt| {
+					if let Some(info) = info_opt.as_mut() {
+						info.owner = new_owner;
+					} else {
+						log::info!("mutate file error: {:?}", last_key);
+					}
+
+					Ok(())
+				})?;
+
+				cursor = Some(last_key);
+			} else {
+				cursor = None;
+				break
+			}
+		}
+
+		Ok(cursor)
+	}
+}
 
 /// A struct that does not migration, but only checks that the counter prefix exists and is correct.
 pub struct MigrationFileBank<T: crate::Config>(sp_std::marker::PhantomData<T>);
@@ -29,7 +97,7 @@ pub fn migrate<T: Config>() -> Weight {
 	let mut weight: Weight = Weight::zero();
 
 	if version < 3 {
-		weight = weight.saturating_add(v3::migrate::<T>());
+		weight = weight.saturating_add(v3_buk::migrate::<T>());
         StorageVersion::new(3).put::<Pallet<T>>();
 	}
 
@@ -37,6 +105,38 @@ pub fn migrate<T: Config>() -> Weight {
 }
 
 mod v3 {
+	use super::*;
+
+	#[storage_alias]
+	pub type File<T: Config> = StorageMap<Pallet<T>, Blake2_128Concat, Hash, OldFileInfo<T>>;
+
+	#[derive(Encode, Decode, TypeInfo)]
+	struct OldUserBrief<T: Config> {
+		pub user: AccountOf<T>,
+		pub file_name: BoundedVec<u8, T::NameStrLimit>,
+		pub bucket_name: BoundedVec<u8, T::NameStrLimit>,
+		pub territory_name: TerrName,
+	}
+
+	#[derive(Encode, Decode, TypeInfo)]
+	struct OldFileInfo<T: Config> {
+		pub(super) segment_list: BoundedVec<SegmentInfo<T>, T::SegmentCount>,
+		pub(super) owner: BoundedVec<OldUserBrief<T>, T::OwnerLimit>,
+		pub(super) file_size: u128,
+		pub(super) completion: BlockNumberFor<T>,
+		pub(super) stat: FileState,
+	}
+
+	#[derive(Encode, Decode, TypeInfo)]
+	struct OldDealInfo<T: Config> {
+		pub(super) file_size: u128,
+		pub(super) segment_list: BoundedVec<SegmentList<T>, T::SegmentCount>,
+		pub(super) user: OldUserBrief<T>,
+		pub(super) complete_list: BoundedVec<CompleteInfo<T>, T::FragmentCount>,
+	}
+}
+
+mod v3_buk {
 	use super::*;
 
 	#[derive(Decode)]
